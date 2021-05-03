06affb13c5be7e9bd06eb4cf414d15b12f45d89f
Amin <m.a.naghiyan02@gmail.com>
2021-05-03 22:31:14 +0430

    add getter and setter functions

--- circular_link_list/main.cpp
+++ circular_link_list/main.cpp
@@ -2,34 +2,60 @@
 using namespace std;
 
 class node{
-    private:
-        int data;
-        node* next;
-        node* prev;
-    public:
-        //add getter and setter functions here
-        /*
-            here
-        */
+private:
+    int data;
+    node* next;
+    node* prev;
+public:
+    void setdata (int Data)
+    {
+        this->data=Data ;
+    }
+
+    int getdata ()
+    {
+        return this->data ;
+    }
+
+    void setnext (node *next)
+    {
+        this->next=next ;
+    }
+
+    node *getnext ()
+    {
+        return this->next;
+    }
+
+    void setprev (node *prev)
+    {
+        this->prev=prev ;
+    }
+
+    node *getprev ()
+    {
+        return this->prev ;
+    }
 };
 class Circular_linked_list{
-    private:
-        int size;
-        node* current;
-    public:
-        Circular_linked_list();
-        ~Circular_linked_list();
-        //add getter and setter functions here
-        /*
+private:
+    int size;
+    node* current;
+public:
+    Circular_linked_list();
+    ~Circular_linked_list();
 
-            here
-        */
-        void push();
-        void pop();
-        void pop(int data);
-        void push_after(int new_data,int pa_data);
-        void push_befor(int new_data,int pb_data);
-        void print();
+    int getsize ()
+    {
+        return size ;
+    }
+
+    void push();
+    void pop();
+    void pop(int data);
+    void push_after(int new_data,int pa_data);
+    void push_befor(int new_data,int pb_data);
+    void print();
 
 
 };
